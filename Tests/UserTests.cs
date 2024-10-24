46eef5c58f4deca4f5cfa3d97e4e313f681dc483
raultorresage <raul.torres@sage.com>
2024-10-24 15:39:02 +0200

    better tests

--- Tests/UserTests.cs
+++ Tests/UserTests.cs
@@ -1,11 +1,33 @@
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using ProductsAPI.Models;
+using System.Net.Http.Json;
+
 namespace Tests
 {
-    public class UserTests
+    public class UserTests: IClassFixture<WebApplicationFactory<Program>>
     {
-        [Fact]
-        public void Test1()
-        {
 
+        private readonly HttpClient _client;
+
+        public UserTests()
+        {
+            WebApplicationFactory<Program> factory = new WebApplicationFactory<Program>();
+            this._client = factory.CreateClient();
+        }
+
+        [Theory]
+        [InlineData("user", "password")]
+        [InlineData("raul", "123")]
+        [InlineData("admin", "admin")]
+        public void UserMustLogIn(string u, string p)
+        {
+            u.Should().NotBeNullOrEmpty();
+            p.Should().NotBeNullOrEmpty();
+
+            User user = new User(u, p);
+            this._client.PostAsJsonAsync("api/user/login", user).Result.Should().HaveStatusCode(System.Net.HttpStatusCode.OK);
         }
     }
 }
